12b88a5a37b3020cd106de559adee7b951a04434
Quote Pilgrim <caio.psv@gmail.com>
2026-01-14 23:00:10 -0300

    turns out I didn't need to count the presents
    I started by adding 1 to each location every time it was visited
    when wiring part 1 just in case I needed to know how many presents
    each house got in an attempt to anticipate what part 2 would ask
    me to do. Turns out it that wasn't necessary so I rewrote both parts
    to just set each location to true whenever it was visited.

---
+++ d3.lua
@@ -0,0 +1,82 @@
+local t = {}
+local result
+
+local function p1(data)
+	local x, y = 0, 0
+	local locations = {}
+
+	for i = 1, #data do
+		locations[x] = locations[x] or {}
+		locations[x][y] = locations[x][y] or true
+		local c = data:sub(i, i)
+
+		if c == "<" then
+			x = x - 1
+		elseif c == ">" then
+			x = x + 1
+		elseif c == "v" then
+			y = y + 1
+		elseif c == "^" then
+			y = y - 1
+		end
+	end
+
+	local result = 0
+	for _, i in pairs(locations) do
+		for _ in pairs(i) do
+			result = result + 1
+		end
+	end
+	return result
+end
+
+local function p2(data)
+	local x, y, rx, ry = 0, 0, 0, 0
+	local locations = {}
+	local moves = {
+		["<"] = { -1, 0 },
+		[">"] = { 1, 0 },
+		["v"] = { 0, 1 },
+		["^"] = { 0, -1 },
+	}
+
+	for i = 1, #data do
+		local move = moves[data:sub(i, i)]
+		if i % 2 == 0 then
+			locations[x] = locations[x] or {}
+			locations[x][y] = locations[x][y] or true
+			x, y = x + move[1], y + move[2]
+		else
+			locations[rx] = locations[rx] or {}
+			locations[rx][ry] = locations[rx][ry] or true
+			rx, ry = rx + move[1], ry + move[2]
+		end
+	end
+
+	local result = 0
+	for _, i in pairs(locations) do
+		for _ in pairs(i) do
+			result = result + 1
+		end
+	end
+	return result
+end
+
+function t.load(part, filename)
+	local file = assert(io.open(filename))
+	local data = file:read()
+
+	if part == "1" then
+		result = p1(data)
+	elseif part == "2" then
+		result = p2(data)
+	end
+
+	return result
+end
+
+function t.draw()
+	love.graphics.print(result or "")
+end
+
+return t
