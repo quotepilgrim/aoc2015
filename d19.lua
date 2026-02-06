7c1448bb5fddce5322d35ac43ee6df8fa86f6bed
Quote Pilgrim <caio.psv@gmail.com>
2026-02-05 23:23:05 -0300

    d9p2

--- d19.lua
+++ d19.lua
@@ -40,6 +40,10 @@ local function find_matches(s, m)
 	return result
 end
 
+local function replace(str, pos, rep)
+	return str:sub(1, pos[1] - 1) .. rep .. str:sub(pos[2] + 1)
+end
+
 M["1"] = function(file)
 	local data, molecule = load_data(file)
 	local result = 0
@@ -48,16 +52,48 @@ M["1"] = function(file)
 	for _, t in ipairs(data) do
 		local matches = find_matches(molecule, t[1])
 		for _, m in ipairs(matches) do
-			local replaced = molecule:sub(1, m[1] - 1) .. t[2] .. molecule:sub(m[2] + 1)
-			unique[replaced] = true
+			unique[replace(molecule, m, t[2])] = true
 		end
 	end
 
-	for k, _ in pairs(unique) do
-		print(k)
+	for _, _ in pairs(unique) do
 		result = result + 1
 	end
 
+	return result
+end
+
+-- this either finds the answer very fast or gets stuck for a very long time
+M["2"] = function(file)
+	local data, molecule = load_data(file)
+	local result = 0
+	local unique = {}
+	local found = false
+
+	unique[molecule] = true
+
+	while not found do
+		result = result + 1
+		for str, _ in pairs(unique) do
+			local new_unique = {}
+			for _, t in ipairs(data) do
+				local matches = find_matches(str, t[2])
+				for _, m in ipairs(matches) do
+					new_unique[replace(str, m, t[1])] = true
+				end
+			end
+
+			for k, _ in pairs(new_unique) do
+				if k == "e" then
+					found = true
+					goto breakout
+				end
+			end
+
+			unique = new_unique
+			::breakout::
+		end
+	end
 	return result
 end
 
