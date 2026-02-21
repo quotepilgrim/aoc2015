8a533ecc98f0f0728f383649672735fd96dcc0a2
Quote Pilgrim <caio.psv@gmail.com>
2026-02-21 10:50:00 -0300

    renaming stuff

--- d23.lua
+++ d23.lua
@@ -17,56 +17,54 @@ local function load_data(file)
 end
 
 local function solve(data, registers)
-	local function execute(pos)
-		local opcode, o1, o2 = unpack(data[pos])
+	local function execute(pc)
+		local ins, op1, op2 = unpack(data[pc])
 
-		if opcode == "hlf" then
-			registers[o1] = math.floor(registers[o1] / 2)
+		if ins == "hlf" then
+			registers[op1] = math.floor(registers[op1] / 2)
 		end
 
-		if opcode == "tpl" then
-			registers[o1] = registers[o1] * 3
+		if ins == "tpl" then
+			registers[op1] = registers[op1] * 3
 		end
 
-		if opcode == "inc" then
-			registers[o1] = registers[o1] + 1
+		if ins == "inc" then
+			registers[op1] = registers[op1] + 1
 		end
 
-		if opcode == "jmp" then
-			return pos + o1
+		if ins == "jmp" then
+			return pc + op1
 		end
 
-		if opcode == "jie" then
-			if registers[o1] % 2 == 0 then
-				return pos + o2
+		if ins == "jie" then
+			if registers[op1] % 2 == 0 then
+				return pc + op2
 			end
 		end
 
-		if opcode == "jio" then
-			if registers[o1] == 1 then
-				return pos + o2
+		if ins == "jio" then
+			if registers[op1] == 1 then
+				return pc + op2
 			end
 		end
 
-		return pos + 1
+		return pc + 1
 	end
 
-	local pos = 1
-	while pos <= #data do
-		pos = execute(pos)
+	local pc = 1
+	while pc <= #data do
+		pc = execute(pc)
 	end
 
 	return registers.b
 end
 
 M["1"] = function(file)
-	local data = load_data(file)
-	return solve(data, { a = 0, b = 0 })
+	return solve(load_data(file), { a = 0, b = 0 })
 end
 
 M["2"] = function(file)
-	local data = load_data(file)
-	return solve(data, { a = 1, b = 0 })
+	return solve(load_data(file), { a = 1, b = 0 })
 end
 
 return M
